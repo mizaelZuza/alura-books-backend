9466f1533efde69e04632a8621c5d49f48c15c62
mizae zuza <mizael.zuza.estudos@gmail.com>
2025-06-02 21:11:51 -0300

    Instalação e configuração da ferramenta axios para permitir a conexão com o front

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const rotaLivro = require("./rotas/rotasLivro.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors({origin: "*"}));
 const PORT = 8000;
 
 app.listen(PORT, () => {
